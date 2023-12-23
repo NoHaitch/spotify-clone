1a174ac99127582be0d6ec43652aa32c878cc49d
Chiko but no H <franciscotrianto@gmail.com>
2023-12-23 20:15:29 +0700

    feat: layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,25 @@
-import type { Metadata } from 'next'
-import { Figtree } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from "next";
+import { Figtree } from "next/font/google";
+import "./globals.css";
+import Sidebar from "@/components/Sidebar";
 
-const font = Figtree({ subsets: ['latin'] })
+const font = Figtree({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Spotify Clone',
-  description: 'Listen to music!',
-}
+  title: "Spotify Clone",
+  description: "Listen to music!",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={font.className}>{children}</body>
+      <body className={font.className}>
+        <Sidebar>{children}</Sidebar>
+      </body>
     </html>
-  )
+  );
 }
